ad2b84edf06a5280598c11d7fa58b3cbacf41af3
heydianaa <dcmndez21@gmail.com>
2026-08-09 02:39:55 -0600

    ISR boton y sensor

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@
 #include <stdint.h>
 #include "driver/ledc.h"
 
-#define boton GPIO_NUM_35
-#define sensor GPIO_NUM_34
+#define boton GPIO_NUM_34
+#define sensor 36
 
 #define ledroja  GPIO_NUM_32
 #define ledverde GPIO_NUM_33
@@ -29,6 +29,16 @@
 #define dispu GPIO_NUM_27
 #define dispdec GPIO_NUM_14
 
+// ADC
+#define ADC_VREF_mV 3300.0
+#define ADC_RESOLUTION 4096.0
+
+// Banderas
+volatile bool leerTemperatura = false;
+volatile uint32_t tiempoBoton = 0;
+const uint32_t tiempoAntirrebote = 50;
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
 // ----Variables----
 
 float temperatura = 0;
@@ -36,11 +46,24 @@ float temperatura = 0;
 
 // ----Funciones----
 void mostrarTemperatura(float temperatura);
-
+void IRAM_ATTR botonISR();
+float leerTemp();
 void setup() {
 
+  Serial.begin(115200);
   gpio_set_direction(boton, GPIO_MODE_INPUT);
-  gpio_set_direction(sensor, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(boton, GPIO_FLOATING);
+  pinMode(sensor, INPUT);
+
+  analogReadResolution(12);
+  analogSetAttenuation(ADC_11db);
+
+  // Interrupcion del boton
+  attachInterrupt(
+    digitalPinToInterrupt((uint8_t)boton),
+    botonISR,
+    CHANGE
+  );
 
   gpio_set_direction(ledroja, GPIO_MODE_OUTPUT);
   gpio_set_direction(ledverde, GPIO_MODE_OUTPUT);
@@ -60,4 +83,54 @@ void setup() {
   gpio_set_level(ledverde, 0);
   gpio_set_level(ledazul, 0);
   mostrarTemperatura(temperatura);
+}
+
+void loop() {
+
+  bool medir = false;
+  // Leer bandera
+  portENTER_CRITICAL(&mux);
+  medir = leerTemperatura;
+  // Limpiar bandera
+  leerTemperatura = false;
+  portEXIT_CRITICAL(&mux);
+  
+  // Si se presiono el boton
+  if (medir) {
+
+    float temperatura = leerTemp();
+    Serial.print(temperatura);
+  }
+}
+
+// Interrupcion del boton
+void IRAM_ATTR botonISR() {
+  uint32_t tiempoActual = millis();
+  uint32_t diferencia = tiempoActual - tiempoBoton;
+  tiempoBoton = tiempoActual;
+
+  if (
+    diferencia >= tiempoAntirrebote &&
+    gpio_get_level(boton) == 1
+  ) {
+    portENTER_CRITICAL_ISR(&mux);
+    leerTemperatura = true;
+    portEXIT_CRITICAL_ISR(&mux);
+  }
+}
+
+//Sensor 
+float leerTemp() {
+
+  // ADC
+  int adcVal = analogRead(sensor);
+
+  // ADC a mV
+  float milliVolt =
+    adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
+
+  // LM35: 10 mV = 1 °C
+  float tempC = milliVolt / 10.0;
+
+  return tempC;
 }
