4242a47a96a192cb340b3c0a6bb800604a2379f9
heydianaa <dcmndez21@gmail.com>
2026-08-10 21:37:24 -0600

    Integrate Adafruit IO for servo position monitoring
    Added Adafruit IO feed for servo position and updated the loop to send servo position data.

--- main.cpp
+++ main.cpp
@@ -47,6 +47,7 @@
 
 // Adafruit IO
 AdafruitIO_Feed *canalTemperatura = io.feed("temperatura");
+AdafruitIO_Feed *canalServoAdafruit = io.feed("servo");
 
 // Banderas
 volatile bool leerTemperatura = false;
@@ -101,6 +102,11 @@ void mostrarNumero(uint8_t numero);
 void apagarDisplays();
 void apagarSegmentos();
 void tareaDisplays(void *parameter);
+
+
+// ========================
+// SETUP
+// ========================
 
 void setup() {
 
@@ -173,7 +179,7 @@ void setup() {
   Serial.println("Sistema listo");
 }
 
-// LOOP
+
 void loop() {
 
   io.run();
@@ -209,8 +215,10 @@ void loop() {
     Serial.println(temperatura);
 
     canalTemperatura->save(temperatura);
+    canalServoAdafruit->save(posicionServo);
   }
 }
+
 
 // INTERRUPCION BOTON
 void IRAM_ATTR botonISR() {
@@ -228,7 +236,7 @@ void IRAM_ATTR botonISR() {
   }
 }
 
-// SENSOR LM35
+
 float leerTemp() {
 
   int adcVal = analogRead(sensor);
@@ -238,20 +246,24 @@ float leerTemp() {
   return tempC;
 }
 
-// REVISAR TEMPERATURA
+
 void revisarTemperatura(float temperatura) {
+
   if (temperatura < 23.0) {
     encenderAzul();
     moverServo(0);
   }
+
   else if (temperatura < 25.0) {
     encenderVerde();
     moverServo(45);
   }
+
   else if (temperatura < 27.0) {
     encenderGelb();
     moverServo(45);
   }
+
   else {
     encenderRojo();
     moverServo(90);
@@ -259,6 +271,7 @@ void revisarTemperatura(float temperatura) {
 }
 
 // CONFIGURAR RGB
+
 void configurarRGB() {
 
   ledc_timer_config_t configTimer = {};
@@ -277,7 +290,9 @@ void configurarRGB() {
 }
 
 void configurarCanal(gpio_num_t pin, ledc_channel_t canal, ledc_timer_t timer) {
+
   ledc_channel_config_t configCanal = {};
+
   configCanal.gpio_num = pin;
   configCanal.speed_mode = LEDC_HIGH_SPEED_MODE;
   configCanal.channel = canal;
@@ -289,7 +304,7 @@ void configurarCanal(gpio_num_t pin, ledc_channel_t canal, ledc_timer_t timer) {
   ledc_channel_config(&configCanal);
 }
 
-// ESCRIBIR RGB
+
 void escribirRGB(uint8_t rojo, uint8_t verde, uint8_t azul) {
 
   ledc_set_duty(LEDC_HIGH_SPEED_MODE, canalRojo, rojo);
@@ -302,33 +317,42 @@ void escribirRGB(uint8_t rojo, uint8_t verde, uint8_t azul) {
   ledc_update_duty(LEDC_HIGH_SPEED_MODE, canalAzul);
 }
 
-// COLORES RGB
+
 void apagarLED() {
   escribirRGB(0, 0, 0);
 }
+
 void encenderRojo() {
   escribirRGB(255, 0, 0);
 }
+
 void encenderVerde() {
   escribirRGB(0, 255, 0);
 }
+
 void encenderAzul() {
   escribirRGB(0, 0, 255);
 }
+
 void encenderGelb() {
   escribirRGB(255, 170, 0);
 }
 
-// SERVO
+
 void configurarServo() {
+
   ledc_timer_config_t configTimerServo = {};
+
   configTimerServo.speed_mode = LEDC_HIGH_SPEED_MODE;
   configTimerServo.duty_resolution = LEDC_TIMER_16_BIT;
   configTimerServo.timer_num = timerServo;
   configTimerServo.freq_hz = 50;
   configTimerServo.clk_cfg = LEDC_AUTO_CLK;
+
   ledc_timer_config(&configTimerServo);
+
   ledc_channel_config_t configCanalServo = {};
+
   configCanalServo.gpio_num = servoPin;
   configCanalServo.speed_mode = LEDC_HIGH_SPEED_MODE;
   configCanalServo.channel = canalServo;
@@ -336,34 +360,44 @@ void configurarServo() {
   configCanalServo.timer_sel = timerServo;
   configCanalServo.duty = 0;
   configCanalServo.hpoint = 0;
+
   ledc_channel_config(&configCanalServo);
 }
 
 void moverServo(int grados) {
+
   grados = constrain(grados, 0, 180);
+
   uint32_t pulso = map(grados, 0, 180, 600, 2400);
   uint32_t duty = (pulso * 65535UL) / 20000UL;
+
   ledc_set_duty(LEDC_HIGH_SPEED_MODE, canalServo, duty);
   ledc_update_duty(LEDC_HIGH_SPEED_MODE, canalServo);
+
   posicionServo = grados;
 }
 
-// SEPARAR TEMPERATURA
+
 void separarTemperatura(float temperatura) {
+
   int temp = (int)(temperatura * 10.0 + 0.5);
+
   if (temp < 0) {
     temp = 0;
   }
+
   if (temp > 999) {
     temp = 999;
   }
+
   decenas = temp / 100;
   unidades = (temp / 10) % 10;
   decimal = temp % 10;
 }
 
-// MOSTRAR NUMERO
+
 void mostrarNumero(uint8_t numero) {
+
   gpio_set_level(a, numeros[numero][0]);
   gpio_set_level(b, numeros[numero][1]);
   gpio_set_level(c, numeros[numero][2]);
@@ -373,8 +407,9 @@ void mostrarNumero(uint8_t numero) {
   gpio_set_level(g, numeros[numero][6]);
 }
 
-// APAGAR SEGMENTOS
+
 void apagarSegmentos() {
+
   gpio_set_level(a, LOW);
   gpio_set_level(b, LOW);
   gpio_set_level(c, LOW);
@@ -384,15 +419,18 @@ void apagarSegmentos() {
   gpio_set_level(g, LOW);
 }
 
-// APAGAR DISPLAYS
+
 void apagarDisplays() {
+
   gpio_set_level(DISPLAY_1, LOW);
   gpio_set_level(DISPLAY_2, LOW);
   digitalWrite(DISPLAY_3, LOW);
 }
 
 // TAREA DISPLAYS
+
 void tareaDisplays(void *parameter) {
+
   while (true) {
 
     // Display 1
@@ -412,6 +450,7 @@ void tareaDisplays(void *parameter) {
     gpio_set_level(DISPLAY_2, HIGH);
 
     vTaskDelay(pdMS_TO_TICKS(1));
+
     // Display 3
     apagarDisplays();
     apagarSegmentos();
